b69890e01c8eedb466b68f443aebdcd208debc82
SleepyPxnda <44275698+SleepyPxnda@users.noreply.github.com>
2025-11-30 14:44:07 +0100

    Remove unused fields from PlayerTable for code cleanup

--- src/main/kotlin/de/cloudypanda/database/PlayerTable.kt
+++ src/main/kotlin/de/cloudypanda/database/PlayerTable.kt
@@ -6,8 +6,6 @@ object PlayerTable : Table("players") {
     val uuid = uuid("uuid")
     val onlineTime = long("online_time").default(0)
     val latestDeathTime = long("latest_death_time").default(0)
-    val canEnterNether = bool("can_enter_nether").default(false)
-    val canEnterEnd = bool("can_enter_end").default(false)
 
     override val primaryKey = PrimaryKey(uuid, name = "PK_Player_UUID")
 }
